fb089820751ebb8060a9fe7e6e0ac88c158233fa
Cédric Hartland <c.hartland@castsoftware.com>
2017-03-13 09:09:49 +0100

    minor updates

--- sources/components/values/Values.jsx
+++ sources/components/values/Values.jsx
@@ -10,7 +10,6 @@ export default class Values extends React.Component {
 
   render() {
     const {scores, values} = this.props;
-    console.log(values)
     return (
       <div className="values">
         <Value score={scores[0].value} title="Total Quality" value={values['60017']} />
